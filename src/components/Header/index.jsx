44fbfa757a5b027e1c3601b1a7593631ad43c232
rafawessling <rafaoening@gmail.com>
2023-07-18 15:59:33 +0200

    feat: Added header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import Logo from '../../assets/logo.svg';
+import Profile from '../../assets/profile.jpeg';
+import './style.css';
+
+export default function Header() {
+    return (
+        <>
+            <div className="container-header">
+                <img className="logo-image" src={Logo} alt="" />
+                <div className="perfil">
+                    <img className="profile-image" src={Profile} alt="" />
+                    <span>Welcome, Rafaela.</span>
+                </div>
+            </div>
+        </>
+    );
+}
