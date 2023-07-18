6aaf169239d274706a4de69566689f262426585e
rafawessling <rafaoening@gmail.com>
2023-07-18 15:54:39 +0200

    feat: Added header component to home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header';
+import './style.css';
+
+function App() {
+    return (
+        <div className="container">
+            <div>
+                <Header />
+            </div>
+        </div>
+    );
+}
+
+export default App;
